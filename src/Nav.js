332105b7dd273cc18895cde6a5b299fdc67a51c9
Marcin Szczodrak <msz@hamster.works>
2024-10-24 13:50:40 -0700

    route fix

--- src/Nav.js
+++ src/Nav.js
@@ -5,7 +5,7 @@ function Nav() {
             <ul>
                 <li><img src="/Logo.svg" alt="Logo" /></li>
                 <li><a href="/" role="button">Homepage</a></li>
-                <li><a href="/about" role="button">About Us</a></li>
+                <li><a href="/#about" role="button">About Us</a></li>
                 <li><a href="/menu" role="button">Menu</a></li>
                 <li><a href="/booking" role="button">Reservations</a></li>
                 <li><a href="/order" role="button">Order Online</a></li>
